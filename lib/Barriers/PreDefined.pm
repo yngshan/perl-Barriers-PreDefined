43acc316eef9b31e6f8d74df52e1366722e85a07
Chylli <chylli@binary.com>
2017-04-12 11:58:18 +0800

    init

---
+++ lib/Barriers/PreDefined.pm
@@ -0,0 +1,47 @@
+package Barriers::PreDefined;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Barriers::PreDefined - Module abstract
+
+=head1 SYNOPSIS
+
+    use Barriers::PreDefined;
+    my $instance = Barriers::PreDefined->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+=head1 AUTHOR
+
+Chylli <chylli@163.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Chylli.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
